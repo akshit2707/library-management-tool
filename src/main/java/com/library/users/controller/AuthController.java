1b208af5fd39738dcd244cf8f43280b468163ac3
Akshit Kumar <akshit2707@Akshits-MacBook-Air.local>
2023-02-20 21:29:03 +0530

    Added controller

--- src/main/java/com/library/users/controller/AuthController.java
+++ src/main/java/com/library/users/controller/AuthController.java
@@ -1,10 +1,22 @@
 package com.library.users.controller;
 
+import com.library.users.entity.UserEntity;
+import com.library.users.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
+//@RequestMapping("/users")
 public class AuthController {
+
+    @Autowired private UserService userService;
+
+    @GetMapping ("/getUser")
+    public UserEntity getUserController(){
+        log.info("data found");
+        return userService.getUser("1");
+    }
 
 }
--- src/main/java/com/library/users/entity/UserEntity.java
+++ src/main/java/com/library/users/entity/UserEntity.java
@@ -1,17 +1,23 @@
 package com.library.users.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Entity
-@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "USER_TABLE")
 public class UserEntity {
+    @Id
     private int id;
     private String firstName;
     private String lastName;
     private String email;
     private String phoneNumber;
-
 }
