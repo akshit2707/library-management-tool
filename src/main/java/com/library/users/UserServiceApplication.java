03c1d5b412f0f04986037058a169e3fe68d15240
Akshit Kumar <akshit2707@Akshits-MacBook-Air.local>
2023-02-22 18:53:38 +0530

    file formatted and eureka added

--- src/main/java/com/library/users/UserServiceApplication.java
+++ src/main/java/com/library/users/UserServiceApplication.java
@@ -3,10 +3,12 @@ package com.library.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableEurekaClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
